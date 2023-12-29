73eedf5dd024f465d9ac0ff7b1bebc7ece3b9a5c
KANEKI <110631781+KanekiEzzam@users.noreply.github.com>
2023-12-30 00:39:33 +0100

    puild

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     webpack: (config, { isServer }) => {
+//         // Add a rule to handle the canvas.node binary module
+//         config.module.rules.push({ test: /\.node$/, use: 'raw-loader' });
+    
+//         // Exclude canvas from being processed by Next.js in the browser
+//         if (!isServer) config.externals.push('canvas');
+//         return config;
+//     },
+// }
+
+// module.exports = nextConfig
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
     webpack: (config, { isServer }) => {
         // Add a rule to handle the canvas.node binary module
         config.module.rules.push({ test: /\.node$/, use: 'raw-loader' });
@@ -10,4 +24,3 @@ const nextConfig = {
     },
 }
 
-module.exports = nextConfig
